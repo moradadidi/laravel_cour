f7d604aa999d18e6751b86b42cbbaa1be2edf7f9
morad <adidimorad7@gmail.com>
2025-04-08 13:18:16 +0100

    changes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,26 +5,19 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
 
 
     public function index() {
-        // $posts=Post::all();
-        $posts=DB::select('select * from posts');
-        // Convert stdClass objects to an array
-        $postsArr = Collection::make(json_decode(json_encode($posts), true));
-        // dd($postsArr);
-        return view('posts.index' , compact('postsArr'));
+        $posts=Post::all();
+        // dd($posts);
+        return view('posts.index' , compact('posts'));
     }
 
     public function show(   $id) {
         $post = Post::findOrFail($id);
-        // $samePosts = DB::select('select * where id = ');
         return view('posts.show', compact('post'));
     }
 
@@ -38,27 +31,97 @@ class PostController extends Controller
                 'email' => 'required',
                 'message' => 'required',
             ]);
-            DB::insert("insert into posts (name,email,message ) values (?, ?, ?)", [$post['name'], $post['email'], $post['message']]);
             // dd($post);
-            // Post::create($post);
-           
+            Post::create($post);
             return redirect()->route('posts.index');
     }
 
     public function edit($id) {
-        $post = DB::select('select * from posts where id = ?',[$id])[0];
-        // dd($post->id);
+        $post = Post::findOrFail($id);
         return view('posts.edit', compact('post'));
     }
 
     public function update(Request $request, $id) {
-        DB::update('update posts  set name = ?, email = ?, message = ? where id = ?', [ $request->name, $request->email, $request->message,$id]);
+        $post = Post::findOrFail($id);
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->author = $request->author;
+        $post->save();
         return redirect()->route('posts.index');
     }
     public function destroy($id) {
-        // $post = Post::findOrFail($id);
-        // $post->delete();
-        DB::delete('delete from posts where id = ?', [$id]);
+        $post = Post::findOrFail($id);
+        $post->delete();
         return redirect()->route('posts.index');
     }
 }  
+
+
+// <?php
+
+// namespace App\Http\Controllers;
+
+// use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+// use App\Models\Post;
+// use Illuminate\Database\Eloquent\Collection;
+// use Illuminate\Support\Arr;
+// use Illuminate\Support\Facades\DB;
+
+// class PostController extends Controller
+// {
+
+
+//     public function index() {
+//         // $posts=Post::all();
+//         $posts=DB::select('select * from posts');
+//         // Convert stdClass objects to an array
+//         $postsArr = Collection::make(json_decode(json_encode($posts), true));
+//         // dd($postsArr);
+//         return view('posts.index' , compact('postsArr'));
+//     }
+
+//     public function show(   $id) {
+//         $post = Post::findOrFail($id);
+//         // $samePosts = DB::select('select * where id = ');
+//         return view('posts.show', compact('post'));
+//     }
+
+//     public function create() {
+//         return view('posts.create');
+//     }
+
+//     public function store(Request $request) {
+//             $post = $request->validate([
+//                 'name' => 'required',
+//                 'email' => 'required',
+//                 'message' => 'required',
+//             ]);
+//             DB::insert("insert into posts (name,email,message ) values (?, ?, ?)", [$post['name'], $post['email'], $post['message']]);
+//             // dd($post);
+//             // Post::create($post);
+           
+//             return redirect()->route('posts.index');
+//     }
+
+//     public function edit($id) {
+//         $post = DB::select('select * from posts where id = ?',[$id])[0];
+//         // dd($post->id);
+//         return view('posts.edit', compact('post'));
+//     }
+
+//     public function update(Request $request, $id) {
+//         DB::update('update posts  set name = ?, email = ?, message = ? where id = ?', [ $request->name, $request->email, $request->message,$id]);
+//         return redirect()->route('posts.index');
+//     }
+//     public function destroy($id) {
+//         // $post = Post::findOrFail($id);
+//         // $post->delete();
+//         DB::delete('delete from posts where id = ?', [$id]);
+//         return redirect()->route('posts.index');
+//     }
+// }  
+
+
+
+
